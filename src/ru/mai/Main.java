c2003fba084e13da99a8e9982cec2abbae9b4399
KathrinBeaver <kathrin.beaver@mail.ru>
2020-04-04 11:42:10 +0300

    Add blanks cleaner

--- src/ru/mai/Main.java
+++ src/ru/mai/Main.java
@@ -1,8 +1,17 @@
 package ru.mai;
 
+import ru.mai.cleaners.BlanksCleaner;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine();
 
+        data = BlanksCleaner.cleanString(data);
+
+        System.out.println(data);
     }
 }
---
+++ src/ru/mai/cleaners/BlanksCleaner.java
@@ -0,0 +1,7 @@
+package ru.mai.cleaners;
+
+public class BlanksCleaner {
+	public static String cleanString(String data) {
+		return data.trim();
+	}
+}
